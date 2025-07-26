8aa907b4a2b3e6f79bbc7ba3f652b9e40db25cc0
SANYOG SHARMA <142544426+sanyog2005@users.noreply.github.com>
2025-07-26 16:39:50 +0530

    timepass

---
+++ day_7_san/bestCase.java
@@ -0,0 +1,30 @@
+package com.day_7_san;
+
+public class bestCase {
+    //if the worst case of 2 algo is same then we calculate the best case of them
+    //ex bubble and selection sort have same worst case but bubble case has a better best case (big omega)
+    //big Omega notation gives the tight lower bound of the given algo
+    //so 0<=cg(n)<=f(n)
+    //so f(n ) is atleast cG=g(n)
+
+
+    //average case
+    //the average performance of algorithm
+    //quick sort averge case is good but worst case is very bad
+
+    //recursive code complexity analysis of recursive case
+    //here we have to do time comp;lextity analysis using no. of instructions executed wrt the input
+    //calling a function is a const operation
+    //the time required to execute a fucniton depends on no. of instrunciton in that funciton
+    //total intruciton = no. of insturction in one function call * toatal no. of funciton calls
+
+     //space complexity
+    //asyptotic analysis of space taken wrt change in input
+    //we donot consider space of input data structure as algo is not creating it ,it is getting it
+    
+}
+
+
+
+
+

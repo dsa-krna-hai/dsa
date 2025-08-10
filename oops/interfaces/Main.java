9bd402b38b7b7046a0726a9389ce754cc6533e69
SANYOG SHARMA <142544426+sanyog2005@users.noreply.github.com>
2025-08-11 02:46:15 +0530

    complete oops

---
+++ oops/interfaces/Main.java
@@ -0,0 +1,22 @@
+package com.company.oops.interfaces;
+
+import com.oops.interfaces.car;
+
+public class Main {
+    public static void main(String[] args) {
+        car c = new car();
+        c.acc();
+        c.display();
+        c.start();
+        engine e = new car();
+        media m = new car();
+
+        engine.san();
+//        e.san(); this will give error
+//        m.san(); this will give error
+
+        media.san();
+//        c.san(); this will give error
+        m.abc();
+    }
+}
